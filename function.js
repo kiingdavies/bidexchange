b7908e6827f4941986d7f6aa0b0c2aa598714a0f
kiingdavies <daviesadedeji007@gmail.com>
2021-03-18 15:18:25 +0100

    Done

--- function.js
+++ function.js
@@ -5,14 +5,14 @@ const from_ammountEl = document.getElementById('from_ammount');
 const to_currencyEl = document.getElementById('to_currency');
 const to_ammountEl = document.getElementById('to_ammount');
 const rateEl = document.getElementById('rate');
-const exchange = document.getElementById('exchange');
+const exchanger = document.getElementById('exchanger');
  
 from_currencyEl.addEventListener('change', calculate);
 from_ammountEl.addEventListener('input', calculate);
 to_currencyEl.addEventListener('change', calculate);
 to_ammountEl.addEventListener('input', calculate);
  
-exchange.addEventListener('click', () => {
+exchanger.addEventListener('click', () => {
 	const temp = from_currencyEl.value;
 	from_currencyEl.value = to_currencyEl.value;
 	to_currencyEl.value = temp;
